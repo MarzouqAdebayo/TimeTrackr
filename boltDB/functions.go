7b668bfa52c79f0a417e7e4cb76afb8d52d311ac
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-09-10 09:26:54 +0100

    Added StartTask functionality

--- boltDB/functions.go
+++ boltDB/functions.go
@@ -3,6 +3,7 @@ package boltdb
 import (
 	"errors"
 	"fmt"
+	"strings"
 	"time"
 )
 
@@ -10,16 +11,21 @@ func Setup() error {
 	return CreateBucket()
 }
 
-func StartTask(taskName string) error {
-	_, err := GetTaskByValue(TaskStatus(ONGOING))
-	if err != nil && !errors.Is(err, ErrTaskNotFound) {
-		return err
+func StartTask(taskName string, category string) error {
+	ongoingErr := OngoingExists()
+	if ongoingErr != nil {
+		return ongoingErr
+	}
+	taskExistsErr := TaskExists(taskName)
+	if taskExistsErr != nil {
+		return taskExistsErr
 	}
 	newTask := &Task{
 		ID:        "00001",
 		Name:      taskName,
+		Category:  category,
 		StartTime: time.Now().Unix(),
-		EndTime:   time.Now().Unix(),
+		EndTime:   0,
 		Status:    TaskStatus(ONGOING),
 	}
 	saveErr := SaveTask(newTask)
@@ -29,27 +35,61 @@ func StartTask(taskName string) error {
 	return nil
 }
 
-func StopCurrentTask() error {
+func StopCurrentTask() (string, error) {
 	task, err := GetTaskByValue(TaskStatus(ONGOING))
-	if err != nil {
-		return err
+	if err != nil && errors.Is(err, ErrTaskNotFound) {
+		return "", fmt.Errorf("No ongoing task")
 	}
+	if err != nil {
+		return "", err
+	}
+	task.EndTime = time.Now().Unix()
 	task.Status = TaskStatus(COMPLETED)
 	saveErr := SaveTask(&task)
 	if saveErr != nil {
-		return err
+		return "", err
 	}
-	return nil
+	return formatTaskStatus(task), nil
 }
 
-func PauseCurrentTask() {
+func PauseCurrentTask() (string, error) {
+	task, err := GetTaskByValue(TaskStatus(ONGOING))
+	if err != nil && errors.Is(err, ErrTaskNotFound) {
+		return "", fmt.Errorf("No ongoing task")
+	}
+	if err != nil {
+		return "", err
+	}
+	task.Status = TaskStatus(PAUSED)
+	task.Duration = task.Duration + time.Now().Unix() - task.StartTime
+	saveErr := SaveTask(&task)
+	if saveErr != nil {
+		return "", err
+	}
+	return formatTaskStatus(task), nil
 }
+
+func ContinuePausedTask(taskName string) (string, error) {
+	return "", nil
+}
+
+//FIX Create a once and for all gettask function that get a task based on different criteria
+func FindTask() {}
 
 func formatTaskStatus(task Task) string {
 	startTime := time.Unix(task.StartTime, 0).Format("2006-01-02 15:04:05")
 	endTime := "N/A"
 	if task.EndTime > 0 {
 		endTime = time.Unix(task.EndTime, 0).Format("2006-01-02 15:04:05")
+	}
+
+	var duration int64
+	if strings.Compare(string(task.Status), string(TaskStatus(ONGOING))) == 0 {
+		duration = time.Now().Unix() - task.StartTime
+	} else if strings.Compare(string(task.Status), string(TaskStatus(PAUSED))) == 0 {
+		duration = task.Duration + time.Now().Unix() - task.StartTime
+	} else {
+		duration = task.EndTime - task.StartTime
 	}
 
 	return fmt.Sprintf(
@@ -69,12 +109,17 @@ func formatTaskStatus(task Task) string {
 		task.Status,
 		startTime,
 		endTime,
-		task.Duration,
+		duration,
 	)
 }
 
-func Status() (string, error) {
-	task, err := GetTaskByValue(TaskStatus(PAUSED))
+func Status(status string) (string, error) {
+	statusMap := map[string]TaskStatus{
+		"ongoing":   TaskStatus(ONGOING),
+		"completed": TaskStatus(COMPLETED),
+		"paused":    TaskStatus(PAUSED),
+	}
+	task, err := GetTaskByValue(statusMap[status])
 	if err != nil {
 		return "", err
 	}
